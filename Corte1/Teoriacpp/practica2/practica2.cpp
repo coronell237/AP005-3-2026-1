da443fc5ade66e1858c33c9dcea43b0ba05b00f8
coronell237 <136186270+coronell237@users.noreply.github.com>
2026-02-11 15:42:36 -0500

    Add files via upload

---
+++ Corte1/Teoriacpp/practica2/practica2.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+int n1=0; 
+int n2=0;
+int s=0;
+cout<<"ingrese un numero 1"<<endl;
+
+cin>>n1;
+
+cout<<"ingrese numero 2"<<endl;
+
+cin>>n2; 
+
+s=n1+n2;
+ 
+cout<<"la sumatoria es: "<<s<<endl;
+
+return 0; 
+}
